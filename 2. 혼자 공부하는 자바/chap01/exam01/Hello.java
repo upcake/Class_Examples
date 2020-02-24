a735698f03fd802fb87dd887d14f636220047e06
upcake <61422279+upcake@users.noreply.github.com>
2020-02-25 00:34:13 +0900

    Create Hello.java

---
+++ 2. 혼자 공부하는 자바/chap01/exam01/Hello.java
@@ -0,0 +1,13 @@
+package sec03.exam01;
+
+/*
+ * 작성자: 홍길동
+ * 작성일: 2020. 02. 24
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	} //end of main
+} //end of class
