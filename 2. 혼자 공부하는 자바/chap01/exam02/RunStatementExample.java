d245ff4530e0f2c0b2b6f26fc10a76c7333e1dcd
upcake <61422279+upcake@users.noreply.github.com>
2020-02-25 00:34:43 +0900

    Create RunStatementExample.java

---
+++ 2. 혼자 공부하는 자바/chap01/exam02/RunStatementExample.java
@@ -0,0 +1,12 @@
+package sec03.exam02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		int x = 1; 					// 변수 x를 선언하고 1을 저장
+		int y = 2; 					// 변수 y를 선언하고 2를 저장
+		int result = x + y; 		// 변수 result를 선언하고 x와 y를 더한 값을 저장
+		System.out.println(result); // 모니터에 출력하는 메소드 호출
+	}
+
+}
