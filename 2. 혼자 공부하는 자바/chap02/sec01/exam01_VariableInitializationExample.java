8526649ac25867d1d49f773d010cb55a0d26a3cc
upcake <61422279+upcake@users.noreply.github.com>
2020-02-25 23:46:33 +0900

    Create exam01_VariableInitializationExample.java

---
+++ 2. 혼자 공부하는 자바/chap02/sec01/exam01_VariableInitializationExample.java
@@ -0,0 +1,15 @@
+package sec01.exam01; // 변수 초기화
+
+public class VariableInitializationExample {
+	public static void main(String[] args) {
+		// 변수  value 선언
+		int value;
+		
+		// 변수 value의 값을 읽고 10을 더하는 산술 연산을 수행
+		// 연산의 결과값을 변수 result의 초기값으로 저장
+		int result = value + 10;
+		
+		// 변수 result 값을 읽고 콘솔에 출력
+		System.out.println(result);
+	}
+}
