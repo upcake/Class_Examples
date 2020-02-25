3015db53a22d5d8fccc3d58b498f0da3d36d12f3
upcake <61422279+upcake@users.noreply.github.com>
2020-02-25 18:25:25 +0900

    Create Hello.java

---
+++ 3. Java의 정석/Ch. 01 시작/Hello.java
@@ -0,0 +1,27 @@
+/*
+ * Date		: 2020. 02. 25
+ * Source	: Hello.java
+ * Author	: 홍길동
+ * Email	: Hong@naver.com
+ */
+
+
+class Hello {
+
+	public static void main(String[] args) /* 프로그램의 시작 */
+	{
+		System.out.println("Hello, world."); // 화면에 글자를 출력한다.
+		System.out.println("Hello, /* 이것은 주석 아님 */ world.");
+		System.out.println("Hello, world. // 이것도 주석 아님");
+
+//		class 클래스이름 {
+			/*
+			 * 	주석을 제외한 모든 코드는 클래스의 블럭 {}내에 작성해야한다.
+			 */
+//		public static void main(String[] args) ; // 메인 메서드의 선언부
+//		{
+			// 실행될 문장들을 적는다.
+//		}
+	}
+
+}
