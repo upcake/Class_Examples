e022b2cd5b2e0b7ddd2005af0c82b4a69daf15e4
upcake <61422279+upcake@users.noreply.github.com>
2020-02-25 23:52:08 +0900

    Create exam08_BooleanExample.java

---
+++ 2. 혼자 공부하는 자바/chap02/sec02/exam08_BooleanExample.java
@@ -0,0 +1,12 @@
+package sec02.exam08;
+
+public class BooleanExample {
+	public static void main(String[] args) {
+		boolean stop = true;
+		if(stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}
+	}
+}
