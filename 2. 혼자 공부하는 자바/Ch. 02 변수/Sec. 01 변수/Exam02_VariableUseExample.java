4ace8876f84d65afae84da6b7bc351a1343f0a4a
upcake <61422279+upcake@users.noreply.github.com>
2020-02-27 20:12:04 +0900

    Add files via upload

---
+++ 2. 혼자 공부하는 자바/Ch. 02 변수/Sec. 01 변수/Exam02_VariableUseExample.java
@@ -0,0 +1,12 @@
+package sec01; // 변수 사용 
+
+public class Exam02_VariableUseExample {
+	public static void main(String[] args) {
+		int hour = 3;
+		int minute = 5;
+		System.out.println(hour + "시간 " + minute + "분");
+		
+		int totalMinute = (hour*60) + minute;
+		System.out.println("총 " + totalMinute + "분");
+	}
+}
