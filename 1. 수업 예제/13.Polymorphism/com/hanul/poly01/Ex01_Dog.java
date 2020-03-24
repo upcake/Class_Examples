b293d516d6641258a68e7049ffe903455224cbc7
upcake <61422279+upcake@users.noreply.github.com>
2020-03-24 19:24:47 +0900

    Delete Ex01_Dog.java

--- 1. 수업 예제/13.Polymorphism/com/hanul/poly01/Ex01_Dog.java
+++
@@ -1,9 +0,0 @@
-package com.hanul.poly01;
-
-public class Ex01_Dog extends Ex01_Animal {		// 하위 클래스 : Animal Class 상속
-	
-	@Override	// 재정의 : 부모 클래스가 정의한 메소드를 수정
-	public void cry() {
-		System.out.println("멍멍");
-	}
-}
