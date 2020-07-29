8f4536d7c00a16f2a2d4d7de93e7d861a425f458
upcake <61422279+upcake@users.noreply.github.com>
2020-07-29 12:53:32 +0900

    Add files via upload

--- 9. Spring/iot/table.sql
+++ 9. Spring/iot/table.sql
@@ -152,3 +152,107 @@ ALTER TRIGGER trg_notice DISABLE;
 SELECT * FROM notice;
 
 COMMIT;
+
+--20/07/20======================================================================================================================
+INSERT INTO member(id, pw, gender, email, name, admin)
+VALUES ('admin2', '1234', '남', 'admin2@admin@.com', '운영자', 'Y');
+
+UPDATE notice SET writer='admin2'
+WHERE mod(id, 3) = 0;
+
+COMMIT;
+
+--qna 테이블 생성
+CREATE TABLE qna(
+    id          NUMBER CONSTRAINT qna_id_pk PRIMARY KEY,
+    title       VARCHAR2(300) NOT NULL,
+    content     VARCHAR2(4000) NOT NULL,
+    writer      VARCHAR2(20) NOT NULL,
+    writedate   DATE DEFAULT SYSDATE,
+    readcnt     NUMBER DEFAULT 0,
+    filename    VARCHAR2(300),
+    filepath    VARCHAR2(300),
+    root        NUMBER,
+    step        NUMBER default 0,
+    indent      NUMBER default 0 
+);
+
+CREATE SEQUENCE seq_qna
+START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER trg_qna
+    BEFORE INSERT ON qna
+    FOR EACH ROW
+BEGIN
+    SELECT seq_qna.NEXTVAL INTO:NEW.id FROM dual;
+END;
+/
+
+INSERT INTO qna (id, title, content, writer)
+VALUES (1, '첫 글 테스트', 'ㅁㄴㅇㄻㄴㅇㄹ', '관리자');
+
+--20/07/21======================================================================================================================
+INSERT INTO qna(title, content, writer, writedate, filepath, filename)
+SELECT title, content, writer, writedate, filepath, filename FROM qna;
+
+UPDATE qna SET root = id;
+
+ALTER TRIGGER trg_qna DISABLE
+
+UPDATE qna SET writer='admin2'
+WHERE mod(id, 3) = 0;
+
+COMMIT;
+
+--방명록 관리
+CREATE TABLE board(
+	id			NUMBER			CONSTRAINT board_id_pk PRIMARY KEY,
+	title		VARCHAR2(300)	NOT NULL,
+	content		VARCHAR2(4000)	NOT NULL,
+	writer		VARCHAR2(100)	NOT NULL,
+	writedate	DATE DEFAULT SYSDATE,
+	readcnt		NUMBER DEFAULT 0,
+	filename	VARCHAR2(300),
+	filepath	VARCHAR2(300)
+);
+
+CREATE SEQUENCE seq_board START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER trg_board
+	BEFORE INSERT ON board
+	FOR EACH ROW
+BEGIN
+	SELECT seq_board.NEXTVAL INTO :new.id FROM dual;
+END;
+/
+
+COMMIT;
+
+--20/07/22========================================================
+INSERT INTO board (title, content, writer, filename, filepath)
+SELECT title, content, writer, filename, filepath
+FROM board;
+
+--20/07/24========================================================
+CREATE TABLE board_comment (
+	id NUMBER constraint board_comment_id_pk PRIMARY KEY,
+	pid NUMBER NOT NULL, /* 원글의 아이디 */
+	writer VARCHAR2(20) NOT NULL, /* 댓글 작성자 아이디 */
+	content VARCHAR2(4000) NOT NULL,
+    writedate DATE DEFAULT SYSDATE,
+    CONSTRAINT board_comment_pid_fk FOREIGN KEY(pid) REFERENCES board(id) ON DELETE CASCADE,
+    CONSTRAINT board_comment_writer_fk FOREIGN KEY(writer) REFERENCES member(id) ON DELETE CASCADE
+);
+
+CREATE SEQUENCE seq_board_comment
+START WITH 1 INCREMENT BY 1;
+
+CREATE OR REPLACE TRIGGER trg_board_comment
+    BEFORE INSERT ON board_comment
+    FOR EACH ROW
+BEGIN
+    SELECT seq_board_comment.NEXTVAL INTO :NEW.id FROM dual;
+END;
+/
+
+COMMIT;
